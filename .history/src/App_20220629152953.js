ba4353ef74fad3964ec15246fec0c639587e4f5e
Jamilsonh <2Jamilson@gmail.com>
2022-06-29 16:20:47 -0300

    Iniciando Projeto

--- .history/src/App_20220629152953.js
+++ .history/src/App_20220629152953.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
-    </Router>
+    </div>
   );
 }
 
--- .history/src/App_20220629155435.js
+++ .history/src/App_20220629155435.js
@@ -4,9 +4,9 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
-    </Router>
+    </div>
   );
 }
 
